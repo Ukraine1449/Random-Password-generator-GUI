a693b96af2d5bbed6d63cc7a874cb7bce23a9dcc
Ukraine1449 <59920083+Ukraine1449@users.noreply.github.com>
2021-06-17 19:44:59 -0400

    Updated letter generation.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ tk = tkinter.Tk()
 tk.title('Random password generator')
 listbox = tkinter.Listbox(tk)
 listbox.insert(END, "Please enter how many of each do you want?")
-listbox.insert(END, "1st box is numbers, 2nd box is special characters and their is letters")
+listbox.insert(END, "3rd box is numbers, 2nd box is special characters and first is letters")
 listbox.insert(END, "Please enter only numbers")
 listbox.pack()
 entry = tkinter.Entry(tk)
@@ -25,7 +25,7 @@ def set_letters():
     randPassword(letters=int(entryy.get()), numbers=int(entry.get()), special=int(entryyy.get()))
 
 def randPassword(letters, special, numbers):
-    capital = string.ascii_letters.upper()
+    capital = string.ascii_letters
     speciall = string.punctuation
     numberss = string.digits
     password = []
